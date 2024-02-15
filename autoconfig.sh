99e52f96a64b3eada4dd5a568cf5c2b14d5aa02a
CG-AA <syankuan@gmail.com>
2024-02-15 23:10:42 +0800

    remove sudo required

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
+cp /etc/letsencrypt/live/hspc4.ddns.net/fullchain.pem .
+cp /etc/letsencrypt/live/hspc4.ddns.net/privkey.pem .
 
-sudo cp /etc/letsencrypt/live/hspc4.ddns.net/fullchain.pem .
-sudo cp /etc/letsencrypt/live/hspc4.ddns.net/privkey.pem .
-
-sudo docker build -t ClubWeb .
-sudo docker run -d -p 80:80 -p 443:443 --name ClubWeb --restart=always -v /etc/letsencrypt:/etc/letsencrypt ClubWeb
+docker build -t ClubWeb .
+docker run -d -p 80:80 -p 443:443 --name ClubWeb --restart=always -v /etc/letsencrypt:/etc/letsencrypt ClubWeb
