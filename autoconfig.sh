346e945238029582022b91304165013a537bdbfb
CG-AA <syankuan@gmail.com>
2024-02-15 23:15:57 +0800

    bug fix

--- autoconfig.sh
+++ autoconfig.sh
@@ -3,5 +3,5 @@
 cp /etc/letsencrypt/live/hspc4.ddns.net/fullchain.pem .
 cp /etc/letsencrypt/live/hspc4.ddns.net/privkey.pem .
 
-docker build -t ClubWeb .
-docker run -d -p 80:80 -p 443:443 --name ClubWeb --restart=always -v /etc/letsencrypt:/etc/letsencrypt ClubWeb
+docker build -t clubweb .
+docker run -d -p 80:80 -p 443:443 --name clubweb --restart=always -v /etc/letsencrypt:/etc/letsencrypt clubweb
